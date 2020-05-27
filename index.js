15da150e4d1f1b86ef5f7f845a32c82308c126c9
javierfurus <javierfurus@gmail.com>
2020-05-27 20:37:42 +0200

    Added collectible orbs and drew the map.

--- index.js
+++ index.js
@@ -6,25 +6,53 @@ const context = mapGen.context;
 const fillMap = mapGen.fillMap;
 const addCharacter = mapGen.addCharacter;
 const printMap = mapGen.printMap;
-const move = moveFunc.move;
 const addMonster = mapGen.addMonster;
 const monsterContext = mapGen.monsterContext;
+// move functions
+const monsterMove = moveFunc.monsterMovement;
+const move = moveFunc.move;
 // Game code
 const speed = 200;
-const map = generateMap(50, 50);
-const mapBackground = generateMap(50, 50);
+const map = generateMap(31, 30);
+const mapBackground = generateMap(31, 30);
 let direction = null;
 fillMap(map, mapBackground);
+
 addCharacter(map, mapBackground);
-addMonster(map, mapBackground, 7, 8);
-addMonster(map, mapBackground, 9, 8);
+addMonster(map, mapBackground, 7, 10);
 printMap(map);
+
+// Standard input
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
 const start = () => {
   move(direction, map, mapBackground);
+};
+let dirPicker = 0;
+setInterval(() => { dirPicker = Math.floor(Math.random() * 4); }, 2000);
+const monsterStart = () => {
+  const directions = ['up', 'down', 'left', 'right']; // 0: up, 1: down, 2: left, 3: right
+  const chosenDir = directions[dirPicker];
+  // Where should a monster go...
+  if (chosenDir === 'right' && (mapBackground[monsterContext.y + 1] === undefined ||
+    mapBackground[monsterContext.x][monsterContext.y + 1].passeable === false)) { // When the wall is to the right
+    dirPicker = 2;
+  }
+  if (chosenDir === 'left' && (mapBackground[monsterContext.y - 1] === undefined ||
+    mapBackground[monsterContext.x][monsterContext.y - 1].passeable === false)) { // When the wall is to the left
+    dirPicker = 3;
+  }
+  if (chosenDir === 'down' && (mapBackground[monsterContext.x + 1] === undefined ||
+    mapBackground[monsterContext.x + 1][monsterContext.y].passeable === false)) { // When the wall is in on the bottom
+    dirPicker = 0;
+  }
+  if (chosenDir === 'up' && (mapBackground[monsterContext.x - 1] === undefined ||
+    mapBackground[monsterContext.x - 1][monsterContext.y].passeable === false)) { // when the wall is on top
+    dirPicker = 1;
+  }
+  monsterMove(chosenDir, map, mapBackground); // let's move!
 };
 stdin.on('data', (key) => {
   if (key === 'q') {
@@ -42,3 +70,4 @@ stdin.on('data', (key) => {
 });
 
 setInterval(start, speed);
+setInterval(monsterStart, speed);
